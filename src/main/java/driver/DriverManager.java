df88a8c149f4ff0775c89ec1d078e27c236ea1f5
Aliaksandr_Dyrykau <aliaksandr_dyrykau@epam.com>
2022-11-10 16:45:19 +0300

    add ReportPortal, logger and listener

--- src/main/java/driver/DriverManager.java
+++ src/main/java/driver/DriverManager.java
@@ -8,17 +8,22 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class DriverManager {
 
     private static WebDriver driver;
+//    To use special driver versions they can be downloaded to the path src/main/resources/drivers/
+//    in this case need to uncomment next lines:
+//    1)
 //    private static final String DRIVERS_PATH = "src\\main\\resources\\drivers\\";
 
     public static WebDriver getDriver() {
         if (driver == null){
             switch (System.getProperty("browser", "chrome")) {
                 case "firefox":
+//                    2) to use firefox driver:
 //                    System.setProperty("webdriver.gecko.driver", DRIVERS_PATH + "geckodriver.exe");
                     WebDriverManager.firefoxdriver().setup();
                     driver = new FirefoxDriver();
                     break;
                 default:
+//                    3) to use Chrome driver
 //                    System.setProperty("webdriver.chrome.driver", DRIVERS_PATH + "chromedriver.exe");
                     WebDriverManager.chromedriver().setup();
                     driver = new ChromeDriver();
--- src/test/java/utils/ReportPortalTestListener.java
+++ src/test/java/utils/ReportPortalTestListener.java
@@ -3,33 +3,34 @@ package utils;
 import com.epam.reportportal.testng.ReportPortalTestNGListener;
 import driver.DriverManager;
 import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-import org.testng.ISuite;
-import org.testng.ITestContext;
 import org.testng.ITestResult;
 
 @Log4j2
 public class ReportPortalTestListener extends ReportPortalTestNGListener {
+//    private Logger log = LogManager.getRootLogger();
     @Override
     public void onTestStart(ITestResult testResult) {
         super.onTestStart(testResult);
-        log.info("RP_MESSAGE#BASE64#{}", "onTestStart " + testResult.getTestName());
+        log.info("ReportPortalTestListener - onTestStart()");
     }
 
     @Override
     public void onTestSuccess(ITestResult testResult) {
         super.onTestSuccess(testResult);
-        log.info("RP_MESSAGE#BASE64#{}", "onTestSuccess " + testResult.getTestName() );
+        log.info("ReportPortalTestListener - onTestSuccess()");
     }
 
     @Override
     public void onTestFailure(ITestResult testResult) {
-        super.onTestFailure(testResult);
-        log.info("RP_MESSAGE#BASE64#{}#{}",
+        log.warn("RP_MESSAGE#BASE64#{}#{}",
                 ((TakesScreenshot) DriverManager
                         .getDriver())
                         .getScreenshotAs(OutputType.BASE64),
                 "TEST " + testResult.getName() + " FAILED");
+        super.onTestFailure(testResult);
     }
 }

c429c4a9b5fc836b0091b3f2205511acd70cda07
Aliaksandr_Dyrykau <aliaksandr_dyrykau@epam.com>
2022-11-12 10:16:00 +0300

    solve problems with jenkins

--- src/test/java/utils/TestListener.java
+++ src/test/java/utils/TestListener.java
@@ -11,14 +11,14 @@ import java.io.File;
 import java.io.IOException;
 
 public class TestListener implements ITestListener {
-//    @Override
-//    public void onTestFailure(ITestResult result) {
-//        File screenshot = ((TakesScreenshot) DriverManager.getDriver()).getScreenshotAs(OutputType.FILE);
-//        try{
-//            FileUtils.copyFile(screenshot, new File(".//target/screenshots/testFail.png"));
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//        ITestListener.super.onTestFailure(result);
-//    }
+    @Override
+    public void onTestFailure(ITestResult result) {
+        File screenshot = ((TakesScreenshot) DriverManager.getDriver()).getScreenshotAs(OutputType.FILE);
+        try{
+            FileUtils.copyFile(screenshot, new File(".//target/screenshots/testFail.png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        ITestListener.super.onTestFailure(result);
+    }
 }
